a20203363495afed5b3fac29e89cb626055db575
IjzerenHein <hrutjes@gmail.com>
2015-01-04 16:11:40 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 famous-flex-truesize-layoutcontroller-demo
-==========================================
+==========
+
+Demonstrates how to calculate the height of multiple true-size renderables using a LayoutController.
+
+[View the live demo here](https://rawgit.com/IjzerenHein/famous-flex-truesize-layoutcontroller-demo/master/dist/index.html)
+
+
+## Content
+
+-	[Source code](./src/main.js)
+
+
+## Build
+
+To build the demo, make sure grunt, webpack and webpack-dev-server are installed globally:
+
+```
+npm install -g grunt
+npm install -g webpack
+npm install -g webpack-dev-server
+```
+
+Run npm to install all dev-dependencies:
+
+```
+npm install
+```
+
+To build the output (dist-folder), run the webpack command:
+
+```
+webpack
+```
+
+
+## Running
+
+To run the demo either open `dist/index.html`
+
+Or use the live-reload server:
+
+```
+grunt serve
+```
+
+
+## Contribute
+
+If you like this project and want to support it, show some love
+and give it a star.
+
+
+## Contact
+- 	@IjzerenHein
+- 	http://www.gloey.nl
+- 	hrutjes@gmail.com
+
+© 2014 - Hein Rutjes
---
+++ src/main.js
@@ -0,0 +1,93 @@
+/**
+ * This Source Code is licensed under the MIT license. If a copy of the
+ * MIT-license was not distributed with this file, You can obtain one at:
+ * http://opensource.org/licenses/mit-license.html.
+ *
+ * @author: Hein Rutjes (IjzerenHein)
+ * @license MIT
+ * @copyright Gloey Apps, 2014
+ */
+
+/*global define, Please, console*/
+/*eslint no-console:0 no-use-before-define:0*/
+
+define(function(require) {
+
+    //<webpack>
+    require('famous-polyfills');
+    require('famous/core/famous.css');
+    require('./styles.css');
+    require('./index.html');
+    //</webpack>
+
+    // please-js
+    require('pleasejs/Please');
+
+    // import dependencies
+    var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
+    var FlexScrollView = require('famous-flex/FlexScrollView');
+    var LayoutController = require('famous-flex/LayoutController');
+    var ListLayout = require('famous-flex/layouts/ListLayout');
+
+    // Initialize
+    var mainContext = Engine.createContext();
+    mainContext.add(_createScrollView());
+    _fillWithTestData();
+
+    //
+    // Create scrollview
+    //
+    var scrollView;
+    function _createScrollView() {
+        scrollView = new FlexScrollView({
+            useContainer: true,
+            flow: false,
+            mouseMove: true
+        });
+        scrollView.on('layoutend', function() {
+            //console.log('FlexScrollView::Layout...');
+        });
+        return scrollView;
+    }
+
+    //
+    // Create true-sized layout-controller
+    //
+    function _createLC(data) {
+        var lc = new LayoutController({
+            layout: ListLayout,
+            size: [undefined, true],
+            dataSource: data
+        });
+        lc.on('layoutend', function() {
+            //console.log('LayoutController::Layout...');
+        });
+        return lc;
+    }
+
+    //
+    // Create a surface
+    //
+    function _createSurface(content) {
+        return new Surface({
+            size: [undefined, true],
+            classes: ['surface'],
+            content: content,
+            properties: {
+                backgroundColor: window.Please.make_color()
+            }
+        });
+    }
+
+    function _fillWithTestData() {
+        scrollView.push(_createLC([
+            _createSurface('short text'),
+            _createSurface('Lorem ipsum dolor sit amet, aperiri adolescens vituperatoribus ne per, everti audire an qui, an mea agam explicari molestiae. Choro detracto vix no. Eu sea sint legimus, cu mazim intellegam mel, id omnes omnium volutpat duo. Habeo elaboraret te eum. An pri idque apeirian, sea commune abhorreant no. Idque fabellas indoctum sed an, an cum duis civibus, ea platonem contentiones eos.'),
+            _createSurface('short text')
+        ]));
+        scrollView.push(_createLC([
+            _createSurface('Lorem ipsum dolor sit amet, aperiri adolescens vituperatoribus ne per, everti audire an qui, an mea agam explicari molestiae. Choro detracto vix no. Eu sea sint legimus, cu mazim intellegam mel, id omnes omnium volutpat duo. Habeo elaboraret te eum. An pri idque apeirian, sea commune abhorreant no. Idque fabellas indoctum sed an, an cum duis civibus, ea platonem contentiones eos.')
+        ]));
+    }
+});
